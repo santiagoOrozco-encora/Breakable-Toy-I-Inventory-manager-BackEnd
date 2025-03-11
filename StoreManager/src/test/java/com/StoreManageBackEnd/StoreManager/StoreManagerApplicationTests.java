1845a830cfaecf9b7716d392aac1a23a2bbd3266
Santiago Guerra <santiago.guerra@ENCEDCSLPLT0653.local>
2025-03-10 19:28:25 -0600

    Setting of the project

---
+++ StoreManager/src/test/java/com/StoreManageBackEnd/StoreManager/StoreManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StoreManageBackEnd.StoreManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
